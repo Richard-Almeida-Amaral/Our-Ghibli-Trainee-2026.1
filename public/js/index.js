f400504551f0622b69167761822b50101592e5c6
DanrleyGD <danrley.dias@estudante.ufjf.br>
2026-05-18 17:12:48 -0300

    feat:adiciona transição

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
-const perfilSidebar = document.querySelector('.perfilSidebar');
+const perfilSidebar = document.querySelector(".perfilSidebar");
 
-perfilSidebar.addEventListener('click', () => {
-  const sidebar = document.querySelector('.sidebar');
-  sidebar.classList.toggle('closed-sidebar');
+perfilSidebar.addEventListener("click", () => {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.classList.toggle("closed-sidebar");
 });
