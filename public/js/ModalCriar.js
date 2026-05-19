cb006df4cd095d2bae144a6c9f91427b3b3cbb99
DanrleyGD <danrley.dias@estudante.ufjf.br>
2026-05-19 04:55:06 -0300

    feat: inicia criação modalCriar

---
+++ public/js/ModalCriar.js
@@ -0,0 +1,10 @@
+function abrirModalCriar(idModal) {
+    const modal = document.getElementById(idModal);
+    modal.style.display = "flex";
+
+}
+
+function fecharModalCriar(idModal) {
+    const modal = document.getElementById(idModal);
+    modal.style.display = "none";
+}
