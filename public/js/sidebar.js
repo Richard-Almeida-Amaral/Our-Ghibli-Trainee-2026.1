1cb26c2271ba616a2bbd6c28979b67049bcd070c
DanrleyGD <danrley.dias@estudante.ufjf.br>
2026-05-19 00:45:30 -0300

    feat: adiciona estrutura html

--- public/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-const perfilSidebar = document.querySelector(".perfilSidebar");
-
-perfilSidebar.addEventListener("click", () => {
-    const sidebar = document.querySelector(".sidebar");
-    sidebar.classList.toggle("closed-sidebar");
-});
