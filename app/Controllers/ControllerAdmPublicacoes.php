6069a44cfa46702fcff552345fc7fbd991bb269a
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-06-04 00:41:50 -0300

    <feat>:tentativa de criar o routes ADM-publicacoes

---
+++ app/Controllers/ControllerAdmPublicacoes.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\App;
+use Exception;
+
+class ControllerAdmPublicacoes
+{
+
+    public function AdmPublicacoes()
+    {
+        return view('admin/ADM-publicacoes');
+    }
+
+}
+ 
