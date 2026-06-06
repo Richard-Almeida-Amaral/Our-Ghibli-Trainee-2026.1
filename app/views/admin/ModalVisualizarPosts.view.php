996e433619c7868d9575cd6e466917a8d76bb36e
pdrandrade <202665068a@estudante.ufjf.br>
2026-06-06 16:48:14 -0300

    feat: adiciona logica de abertura e fechamento de modais de edição e visualização de posts

--- app/views/admin/ModalVisualizarPosts.view.php
+++ app/views/admin/ModalVisualizarPosts.view.php
@@ -28,8 +28,7 @@
 <body>
 
 <!-- botoes abrir modais -->
-  <section id="botaoabrirviewpost">Visualizar Post</section>
-  <section id="botaoabrireditpost">Editar Post</section>
+
 
 <!-- modal visualizar post -->
  <div id="filtromodalview">
@@ -37,11 +36,11 @@
 
       <section id="frametitulomodalview">
         <div id="titulomodalview">Visualização de Publicação</div>
-        <div id="containerxmodalviewpost">
+        <button id="containerxmodalviewpost" onclick="fecharModal('containermodalview','filtromodalview')">
         <svg xmlns="http://www.w3.org/2000/svg" class="xmodalviewpost" viewBox="0 0 16 16">
         <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
         </svg>
-      </div>
+        </button>
       </section>
       
       <section id="conteudomodalview">
@@ -96,8 +95,8 @@
       </section>
 
       <section id="botoesmodaledit">
-        <div id="botaosalvaredit">Salvar e sair</div>
-        <div id="botaocancelaredit">Cancelar</div>
+        <button id="botaosalvaredit">Salvar e sair</button>
+        <button id="botaocancelaredit" onclick="fecharModal('containermodaledit','filtromodaledit')">Cancelar</button>
       </section>
   </div>
 </div>
--- public/js/ModalVisualizarPosts.js
+++ public/js/ModalVisualizarPosts.js
@@ -1,28 +1,16 @@
-const botaoabreeditpost = document.getElementById("botaoabrireditpost");
-const botaoabreviewpost = document.getElementById("botaoabrirviewpost");
-const containermodaleditpost = document.getElementById("containermodaledit");
-const containermodalviewpost = document.getElementById("containermodalview");
-const filtromodaledit = document.getElementById("filtromodaledit");
-const filtromodalview = document.getElementById("filtromodalview");
-const botaofecharmodaledit = document.getElementById("botaocancelaredit");
-const botaofecharmodalview = document.getElementById("containerxmodalviewpost")
 
-botaoabreeditpost.addEventListener("click", () => {
-    filtromodaledit.classList.add("aberto");
-    containermodaleditpost.classList.add("aberto");
-})
+function abrirModal(idModal, idFiltroModal){
+    const modal = document.getElementById(idModal);
+    const filtromodal = document.getElementById(idFiltroModal);
+    modal.classList.add("aberto");
+    filtromodal.classList.add("aberto");
 
-botaofecharmodaledit.addEventListener("click", () => {
-    containermodaleditpost.classList.remove("aberto");
-    filtromodaledit.classList.remove("aberto");
-})
+}
 
-botaoabreviewpost.addEventListener("click", () => {
-    filtromodalview.classList.add("aberto");
-    containermodalviewpost.classList.add("aberto");
-})
+function fecharModal(idModal, idFiltroModal){
+    const modal = document.getElementById(idModal);
+    const filtromodal = document.getElementById(idFiltroModal);
+    modal.classList.remove("aberto");
+    filtromodal.classList.remove("aberto");
 
-botaofecharmodalview.addEventListener("click", () => {
-    containermodalviewpost.classList.remove("aberto");
-    filtromodalview.classList.remove("aberto");
-})
+}
