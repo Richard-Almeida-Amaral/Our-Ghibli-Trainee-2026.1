791b08fb21ce8a8100ce8b2749cbc1186998756d
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-06-07 04:08:49 -0300

    <feat>: Implementacao do HTML e CSS incompleto

--- public/js/ADM-publicacoes.js
+++ public/js/ADM-publicacoes.js
@@ -1,7 +1,7 @@
 
+const screenposts = document.getElementById('auxposts');
 
-
-function ModalDarlan(idModal) {
+function InteracaoModal(idModal) {
     const modal = document.getElementById(idModal);
     if(modal.style.display == 'none'){
         modal.style.display = "block";
@@ -9,3 +9,14 @@ function ModalDarlan(idModal) {
         modal.style.display = "none";
     }
 }
+
+screenposts.addEventListener("click", function (){
+    const modalcreate = document.getElementById(ModalCriar);
+    const modaledit = document.getElementById(containermodalview);
+    const modalview = document.getElementById(ModalExcluir);
+    const modaldelete = document.getElementById(containermodaledit);
+    modalcreate.style.display = "none";
+    modaledit.style.display = "none";
+    modalview.style.display = "none";
+    modaldelete.style.display = "none";
+});
