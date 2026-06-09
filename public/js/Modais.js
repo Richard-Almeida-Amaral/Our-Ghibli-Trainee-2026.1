797dcdce306bdf6adaba5a77862ab6331a96100c
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-06-08 23:39:30 -0300

    <refactor>:Uniao dos modais JS

--- public/js/Modais.js
+++ public/js/Modais.js
@@ -1,70 +1,13 @@
-// Modal Criar
-function AbrirModal(idModal) {
+function abrirModal(idModal, idFiltroModal) {
     const modal = document.getElementById(idModal);
+    const filtromodal = document.getElementById(idFiltroModal);
     modal.style.display = "flex";
-
+    filtromodal.style.display = "flex";
 }
 
-function FecharModal(idModal) {
-    const modal = document.getElementById(idModal);
-    modal.style.display = "none";
-}
-
-function abrirModal(idModal, idFiltroModal){
+function fecharModal(idModal, idFiltroModal) {
     const modal = document.getElementById(idModal);
     const filtromodal = document.getElementById(idFiltroModal);
-    modal.classList.add("aberto");
-    filtromodal.classList.add("aberto");
-
-}
-
-function fecharModal(idModal, idFiltroModal){
-    const modal = document.getElementById(idModal);
-    const filtromodal = document.getElementById(idFiltroModal);
-    modal.classList.remove("aberto");
-    filtromodal.classList.remove("aberto");
-
-}
-
-function fecharModalVisualizar(idModal) {
-    const modal = document.getElementById(idModal);
     modal.style.display = "none";
-
-    const inputs = modal.querySelectorAll('input');
-    inputs.forEach(input => {
-        input.value = '';
-    });
-    
-    const passwordInput = document.getElementById('userSenha');
-    if(passwordInput) {
-        passwordInput.setAttribute('type', 'password');
-    }
-
-    const togglePassword = document.getElementById('togglePassword');
-    if (togglePassword){
-        togglePassword.classList.remove('fa-eye');
-        togglePassword.classList.add('fa-eye-slash');
-    }
+    filtromodal.style.display = "none";
 }
-
-document.addEventListener('DOMContentLoaded', () =>{
-    const togglePassword = document.getElementById('togglePassword');
-    const passwordInput = document.getElementById('userSenha');
-
-
-    if (togglePassword && passwordInput){
-        togglePassword.addEventListener('click', () => {
-            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
-            passwordInput.setAttribute('type', type);
-
-            if(type === 'password'){
-                togglePassword.classList.remove('fa-eye');
-                togglePassword.classList.add('fa-eye-slash');
-            }
-            else {
-                togglePassword.classList.remove('fa-eye-slash');
-                togglePassword.classList.add('fa-eye');
-            }
-        });
-    }
-});
