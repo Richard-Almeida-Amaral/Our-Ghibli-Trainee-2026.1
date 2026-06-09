96b389f765cccde5aafb266847f51dade67c68c1
Danrlygd <danrley.dias@estudante.ufjf.br>
2026-06-09 02:55:17 -0300

    fix: arruma caminho UsuariosController

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -10,7 +10,32 @@ class UsuariosController
 
     public function index()
     {
-        return view('admin/tabelaUsuarios');
+        $usuarios = App:: get('database')->selectAll('usuarios'); 
+
+        return view('admin/tabelaUsuarios', compact('usuarios'));
+    }
+
+    public function criar()
+    {
+        $parameters = [
+            'nome' => $_POST['nome'],
+            'email' => $_POST['email'],
+            'senha' => $_POST['senha'],
+            'imagem' => $_POST['imagem']
+        ];
+
+        App::get('database')->insert('usuarios', $parameters);
+
+        header('Location: /tabelaUsuarios');
+    }
+
+    public function delete()
+    {
+        $id = $_POST['id'];
+
+        App::get('database')->delete('usuarios', $id);
+
+        header('Location: /tabelaUsuarios');
     }
 
 }
