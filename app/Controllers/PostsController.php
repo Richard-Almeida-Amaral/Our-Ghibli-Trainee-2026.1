f043da1fa15338a197fc66ac2ae518cc2176f4a0
mariaclmury <mariaclaramury@gmail.com>
2026-06-16 00:30:41 -0300

    feat: Post Controller

--- app/Controllers/PostsController.php
+++ app/Controllers/PostsController.php
@@ -5,12 +5,64 @@ namespace App\Controllers;
 use App\Core\App;
 use Exception;
 
-class PostsController
+class PostController
 {
 
     public function index()
     {
-        $publicacoes =  App ::get('database')->selectJoinADMP('posts','usuarios');
-        return view('admin/tabelaPublicacoes',compact('publicacoes'));
+        $publicacoes =  App::get('database')->selectJoinADMP('posts', 'usuarios');
+        return view('admin/tabelaPublicacoes', compact('publicacoes'));
+    }
+
+    public function criar()
+    {
+
+        $temporario = $_FILES['imagem']['tmp_name'];
+        $nomeimagem = sha1(uniqid($_FILES['imagem']['name'], true)) . "." . pathinfo($_FILES['imagem']['name'], PATHINFO_EXTENSION);
+        $caminhoimagem = "public/assets/ " . $nomeimagem;
+
+        move_uploaded_file($temporario, $caminhoimagem);
+
+        $parameters = [
+            'titulo' => $_POST['titulo'],
+            'descricao' => $_POST['descricao'],
+            'imagem' => $caminhoimagem,
+            'data' => $_POST['data'],
+            'usuarios_id' => 1        // $_POST['usuarios_id'] usar apos login
+        ];
+
+        App::get('database')->insert('posts', $parameters);
+
+        header('Location: /admin/posts');
+    }
+
+    public function edit()
+    {
+
+    $id = $_POST['id'];
+
+    $post = App::get('database')->selectOne('posts', $id);
+
+    $caminhoimagem = $post->imagem;
+
+    $parameters = [
+            'titulo' => $_POST['titulo'],
+            'descricao' => $_POST['descricao'],
+            'imagem' => $caminhoimagem,
+            'data' => $_POST['data'],
+            'usuarios_id' => 1        // $_POST['usuarios_id'] usar apos login
+        ];
+
+        App::get('database')->update('posts', $id, $parameters);
+        header('Location: /admin/posts');
+    }
+
+
+    public function delete()
+    {
+        $id = $_POST['id'];
+        App::get('database')->delete('posts', $id);
+
+        header('Location: /admin/posts');
     }
 }
