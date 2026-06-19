e92adbcc599e953d06a35a37209a5f6d3c0ea114
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-06-19 00:03:34 -0300

    <fix>:Modal Criar feito

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -28,7 +28,7 @@ class PostController
             'descricao' => $_POST['descricao'],
             'imagem' => $caminhoimagem,
             'data' => $_POST['data'],
-            'usuarios_id' => $_POST['usuarios_id'] //usar apos login
+            'usuarios_id' => $_SESSION['id'] //usar apos login
         ];
 
         App::get('database')->insert('posts', $parameters);
