fd1faa22e244a9c14b2c30b6d672f4f8aa7fed04
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-06-21 01:46:28 -0300

    <feat>:Construcao do modal login

--- app/views/admin/modaisUsuarios/LoginCriar.view.php
+++ app/views/admin/modaisUsuarios/LoginCriar.view.php
@@ -9,23 +9,23 @@
 
     <div class="cadastroBody">
         <div class="cadastroFields">
-            <label for="">Nome</label>
+            <label for="" class="cadastroLabel">Nome</label>
             <input type="text" name="nome" id="" placeholder="Digite o seu nome" class="cadastroInput">
         </div>
         
         <div class="cadastroFields">
-            <label for="">Email</label>
+            <label for="" class="cadastroLabel">Email</label>
             <input type="email" name="email" id="" placeholder="Digite o seu email" class="cadastroInput">
         </div>
         
         <div class="cadastroFields">
-            <label for="">Senha</label>
+            <label for="" class="cadastroLabel">Senha</label>
             <input type="password" name="senha" id="" placeholder="Digite a sua senha" class="cadastroInput">
         </div>
     </div>
 
     <div class="cadastroBtn">
-        <button type="button" class="" onclick="fecharModal('usuarioCriar')">Cancelar</button>
-        <button class="">Criar</button>
+        <button class="cadastroCriar">Criar</button>
+        <button type="button" class="cadastroCancelar" onclick="fecharModal('usuarioCriar')">Cancelar</button>
     </div>
 </form>
